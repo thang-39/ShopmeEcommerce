f7f4396c6195df7e6bfe6249160b37829d684f71
thang-39 <trantruongminhthang@gmail.com>
2023-04-03 17:26:04 +0700

    bla

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -0,0 +1,36 @@
+package com.shopme.common.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    private int id;
+    private String name;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
