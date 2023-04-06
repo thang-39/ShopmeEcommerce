972367533b482bffd887a110b279a4e5f87acf51
thang-39 <trantruongminhthang@gmail.com>
2023-04-06 11:29:33 +0700

    finished create user html page without upload picture

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,11 +1,14 @@
 package com.shopme.admin.user;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -29,8 +32,24 @@ public class UserController {
     }
 
     @GetMapping("/users/new")
-    public String newUser() {
+    public String newUser(Model model) {
+        List<Role> listRoles = service.listRoles();
+        User user = new User();
+        user.setEnabled(true);
+
+        model.addAttribute("user",user);
+        model.addAttribute("listRoles",listRoles);
+
         return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+        System.out.println(user);
+        service.save(user);
+
+        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully");
+        return "redirect:/users";
     }
 
 
