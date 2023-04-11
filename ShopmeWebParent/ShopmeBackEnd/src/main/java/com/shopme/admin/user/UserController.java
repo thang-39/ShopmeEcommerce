b796b7578d7641f8945d2a20f8eb0fae82c49ccc
thang-39 <trantruongminhthang@gmail.com>
2023-04-11 17:34:13 +0700

    add photo

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,18 +1,20 @@
 package com.shopme.admin.user;
 
+import com.shopme.admin.FileUploadUtil;
 import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class UserController {
@@ -48,11 +50,28 @@ public class UserController {
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user, RedirectAttributes redirectAttributes) {
-        System.out.println(user);
-        service.save(user);
+    public String saveUser(User user,
+                           RedirectAttributes redirectAttributes,
+                           @RequestParam("image") MultipartFile multipartFile) throws IOException {
+        if (!multipartFile.isEmpty()) {
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            user.setPhotos(fileName);
+
+            User savedUser = service.save(user);
+
+            String uploadDir = "user-photos/" + savedUser.getId();
+            // clean all photos
+            FileUploadUtil.cleanDir(uploadDir);
+
+            // then save new updated photo
+            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        } else {
+            if (user.getPhotos().isEmpty()) user.setPhotos(null);
+            service.save(user);
+        }
 
         redirectAttributes.addFlashAttribute("message", "The user has been saved successfully");
+
         return "redirect:/users";
     }
 
